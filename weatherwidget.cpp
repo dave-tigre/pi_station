0926f21a89ce84f30c1e09c0f6eb55c27561dfe8
dave-tigre <tigrerosd@gmail.com>
2021-02-04 21:42:42 -0500

    added init of weather widget with framework for calling weather using openweather api

--- weatherwidget.cpp
+++ weatherwidget.cpp
@@ -1,27 +1,82 @@
 #include "weatherwidget.h"
 #include "ui_weatherwidget.h"
 
+
+
+
 WeatherWidget::WeatherWidget(QWidget *parent) :
     QWidget(parent),
-    ui(new Ui::WeatherWidget)
+    ui(new Ui::WeatherWidget),
+    mIconLabel(this),
+    mWeatherImage(new QPixmap())
+
 {
     ui->setupUi(this);
     ui->weatherLabel->setText("Current Weather:");
 
     QString url = "/home/dave-tigre/Documents/git/pi_station/assets/sunny.png";
-    weatherImage = new QPixmap(url);
-    // = weatherImage->scaled(5, 5, Qt::KeepAspectRatio, Qt::SmoothTransformation);
-    QLabel *label = new QLabel(this);
+    mWeatherImage->load(url);
 
-    label->setPixmap(*weatherImage);
+    mIconLabel.setPixmap(mWeatherImage->scaled(mImageXScale,mImageYScale));
 
     QHBoxLayout *layout = new QHBoxLayout(this);
-    layout->addWidget(label);
-    ui->weatherFrame->setLayout(layout);
+    layout->addWidget(&mIconLabel);
+    ui->weatherWidget->setLayout(layout);
+
+    updateWeather();
 
 }
 
 WeatherWidget::~WeatherWidget()
 {
+    curl_global_cleanup();
     delete ui;
+}
+
+void WeatherWidget::requestWeather(WeatherReport &inWeatherReport)
+{
+    inWeatherReport.weatherId = 800;
+    inWeatherReport.mainWeather = "Clear";
+    inWeatherReport.description = "clear sky";
+    inWeatherReport.icon = "01d";
+    inWeatherReport.temp = 282.55;
+    inWeatherReport.humidity = 100;
+    inWeatherReport.windSpeed = 1.5;
+}
+
+size_t WeatherWidget::weatherCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+
+void WeatherWidget::updateWeather()
+{
+    struct WeatherReport latestReport;
+    
+    // curl_global_init(CURL_GLOBAL_ALL);
+    
+    /* init the curl session */ 
+    curl = curl_easy_init();
+
+    if (curl)
+    {
+        curl_easy_setopt(curl, CURLOPT_URL, "api.openweathermap.org/data/2.5/weather?q=Philadelphia,pa,us&mode=xml&appid=c914103bc3250971289b1634cebc8efc");
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, weatherCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+        res = curl_easy_perform(curl);
+
+    }
+
+   std::cout << readBuffer << std::endl;
+    
+    
+    /* cleanup curl stuff */ 
+    curl_easy_cleanup(curl);
+}
+
+std::string WeatherWidget::getWeatherIconImageLocation(const std::string inWeatherIcon)
+{
+    if (inWeatherIcon == "01d") return "sunny.png";
+    else return "ERROR";
 }
--- weatherwidget.h
+++ weatherwidget.h
@@ -1,11 +1,31 @@
 #ifndef WEATHERWIDGET_H
 #define WEATHERWIDGET_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <curl/curl.h>
+#include <iostream>
+
 #include <QWidget>
+#include <QLabel>
 
 namespace Ui {
 class WeatherWidget;
 }
+
+struct WeatherReport
+{
+    int weatherId;
+    std::string mainWeather;
+    std::string description;
+    std::string icon;
+    float temp;
+    float humidity;
+    float windSpeed;
+
+};
+
 
 class WeatherWidget : public QWidget
 {
@@ -15,9 +35,30 @@ public:
     explicit WeatherWidget(QWidget *parent = nullptr);
     ~WeatherWidget();
 
+
+
+// private slots:
+//     void updateWeather();
+
+private:
+    void requestWeather(WeatherReport &inWeatherReport);
+    std::string getWeatherIconImageLocation(const std::string inWeatherIcon);
+    static size_t weatherCallback(void *contents, size_t size, size_t nmemb, void *userp);
+    void updateWeather();
+
 private:
     Ui::WeatherWidget *ui;
-    QPixmap *weatherImage;
+    QPixmap *mWeatherImage;
+    QLabel mIconLabel;
+
+    const int mImageXScale = 70;
+    const int mImageYScale = 70;
+    std::string readBuffer;
+
+    // WeatherReport mCurrentWeather;
+
+    CURL *curl;
+    CURLcode res;
 };
 
 #endif // WEATHERWIDGET_H
